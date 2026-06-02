04b247d60255de4c637a37d3389c6cee17c436b2
0611malhar <malhar0611@gmail.com>
2026-06-02 17:10:59 +0530

    Add files via upload

---
+++ 21_sticks.py
@@ -0,0 +1,10 @@
+import random
+n = 21
+while n > 4:
+    u = int(input("Enter the number(1-4)"))
+    if u in range(1,5):
+        n -= u
+    else:
+        print("invalid input")
+    c = random.randint(1,5)
+    n -= c
