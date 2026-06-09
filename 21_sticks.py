7c2c0dec018651d99f115aea991b3b4b431478cb
0611malhar <malhar0611@gmail.com>
2026-06-09 15:13:25 +0530

    Refactor stick picking game logic
    Refactored the game logic to allow player and computer to pick sticks alternately until one loses.

--- 21_sticks.py
+++ 21_sticks.py
@@ -1,15 +1,20 @@
-import random
-n = 21
-print("""
-rules are :
-the last person who pick the sticks get lost
-0 to 4 are the no of sticks which u pick
-""")
-while n > 4:
-    u = int(input("Enter the number(1-4)"))
-    if u in range(1,5):
-        n -= u
-    else:
-        print("invalid input")
-    c = random.randint(1,5)
-    n -= c
+sticks = 21
+
+while sticks > 0:
+    print("\nSticks left:", sticks)
+    player = int(input("Pick 1 to 4 sticks: "))
+
+    if player < 1 or player > 4:
+        print("Invalid choice!")
+        continue
+    sticks -= player
+    if sticks <= 0:
+        print("You picked the last stick. You lose!")
+        break
+    computer = 5 - player
+    print("Computer picked:", computer)
+    sticks -= computer
+    if sticks <= 0:
+        print("Computer picked the last stick. Computer loses!")
+        print("You win!")
+        break
