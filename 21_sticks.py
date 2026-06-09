34eb00078b2eadeb697f40c966a9a3df62490cfb
namansharma2707 <sh.naman2707@gmail.com>
2026-06-09 14:25:11 +0530

    Update 21_sticks.py

--- 21_sticks.py
+++ 21_sticks.py
@@ -1,5 +1,10 @@
 import random
 n = 21
+print("""
+rules are :
+the last person who pick the sticks get lost
+0 to 4 are the no of sticks which u pick
+""")
 while n > 4:
     u = int(input("Enter the number(1-4)"))
     if u in range(1,5):
