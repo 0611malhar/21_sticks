2b8a799f5d9ee43223f77cc0f8ea636b81a0b259
0611malhar <malhar0611@gmail.com>
2026-07-15 18:02:01 +0530

    Fix input validation and game logic for stick game

--- 21_sticks.py
+++ 21_sticks.py
@@ -2,19 +2,25 @@ sticks = 21
 
 while sticks > 0:
     print("\nSticks left:", sticks)
+
     player = int(input("Pick 1 to 4 sticks: "))
 
-    if player < 1 or player > 4:
+    if player < 1 or player > 4 or player > sticks:
         print("Invalid choice!")
         continue
+
     sticks -= player
-    if sticks <= 0:
+
+    if sticks == 0:
         print("You picked the last stick. You lose!")
         break
-    computer = 5 - player
+
+    computer = min(5 - player, sticks)
     print("Computer picked:", computer)
+
     sticks -= computer
-    if sticks <= 0:
+
+    if sticks == 0:
         print("Computer picked the last stick. Computer loses!")
         print("You win!")
         break
